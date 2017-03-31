7765d33d7ef92da089a20cdd8ec43b56a3d38102
arp226 <arp226@Anjali>
2017-03-31 00:11:32 -0400

    Created method body for create/open/close

--- src/database/MySqlDB.java
+++ src/database/MySqlDB.java
@@ -3,6 +3,12 @@
  */
 package database;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -15,16 +21,39 @@ import food.FoodIntakeType;
  */
 public class MySqlDB implements Database {
 
-	/* (non-Javadoc)
+	Connection connection = null;
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#create(food.FoodIntake)
 	 */
+
 	@Override
 	public boolean create(FoodIntake foodIntake) {
-		// TODO Auto-generated method stub
-		return false;
+		open();
+		// the mysql insert statement
+		String query = " insert into FoodIntake (Name, Date)" + " values (?, ?)";
+
+
+		// create the mysql insert preparedstatement
+		PreparedStatement preparedStmt;
+		try {
+			preparedStmt = connection.prepareStatement(query);
+			preparedStmt.setString(1, foodIntake.getName());
+			preparedStmt.setDate(2, foodIntake.getDate());
+			preparedStmt.execute();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+
+		return close();
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#create(food.FoodIntakeType)
 	 */
 	@Override
@@ -33,8 +62,11 @@ public class MySqlDB implements Database {
 		return false;
 	}
 
-	/* (non-Javadoc)
-	 * @see database.Database#read(java.lang.String, java.util.Date, java.util.Date)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see database.Database#read(java.lang.String, java.util.Date,
+	 * java.util.Date)
 	 */
 	@Override
 	public List<FoodIntake> read(String foodName, Date from, Date to) {
@@ -42,7 +74,9 @@ public class MySqlDB implements Database {
 		return null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#update(food.FoodIntake)
 	 */
 	@Override
@@ -51,7 +85,9 @@ public class MySqlDB implements Database {
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#delete(food.FoodIntake)
 	 */
 	@Override
@@ -60,22 +96,46 @@ public class MySqlDB implements Database {
 		return false;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#open()
 	 */
 	@Override
 	public boolean open() {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("MySql JDBC driver not found");
+			e.printStackTrace();
+			return false;
+		}
+
+		try {
+			connection = DriverManager.getConnection("jdbc:mysql://sql2.njit.edu/arp226", "arp226", "5WucrXW9e");
+
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return false;
+		}
+		return connection != null;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see database.Database#close()
 	 */
 	@Override
 	public boolean close() {
-		// TODO Auto-generated method stub
-		return false;
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+		return true;
 	}
 
 }
