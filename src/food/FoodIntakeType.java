c6ae6a67ec0c3f0d1a2dde4fcaa22b436f6f2e7a
arp226 <arp226@Anjali>
2017-04-07 01:40:07 -0400

    created Enum - Meal Type

--- src/food/FoodIntakeType.java
+++ src/food/FoodIntakeType.java
@@ -1,12 +1,23 @@
-/**
- * 
- */
 package food;
 
-/**
- * @author arp226
- *
- */
-public class FoodIntakeType {
+public enum  FoodIntakeType {
+	DUMMY(""),
+	BREAKFAST("Breakfast"),
+	LUNCH("Lunch"),
+    DINNER("Dinner"),
+    SNACKS("Snacks"),
+    PARTYMEAL("PartyMeal"),
+    MEAL("Meal"),
+    OTHERS("Others"),
+    ;
 
+    private String mealNames;
+
+    FoodIntakeType(String mealNames) {
+        this.mealNames = mealNames;
+    }
+
+    public String mealName() {
+        return mealNames;
+    }
 }
